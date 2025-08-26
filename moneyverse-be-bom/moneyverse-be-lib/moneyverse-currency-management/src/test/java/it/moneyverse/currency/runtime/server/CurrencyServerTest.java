9db986fa7db54002e239fef3b1b656d316c16bdd
Lorenzo Fratini <lorenzo.fratini.developer@gmail.com>
2025-08-26 14:18:29 +0200

    [frontend] feat: added test currency

--- moneyverse-be-bom/moneyverse-be-lib/moneyverse-currency-management/src/test/java/it/moneyverse/currency/runtime/server/CurrencyServerTest.java
+++ moneyverse-be-bom/moneyverse-be-lib/moneyverse-currency-management/src/test/java/it/moneyverse/currency/runtime/server/CurrencyServerTest.java
@@ -1,6 +1,7 @@
 package it.moneyverse.currency.runtime.server;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 import io.grpc.ManagedChannel;
 import io.grpc.Server;
@@ -9,18 +10,24 @@ import io.grpc.inprocess.InProcessServerBuilder;
 import it.moneyverse.core.boot.*;
 import it.moneyverse.currency.model.CurrencyTestContext;
 import it.moneyverse.currency.model.entities.Currency;
+import it.moneyverse.currency.model.entities.ExchangeRate;
 import it.moneyverse.currency.model.repositories.CurrencyRepository;
 import it.moneyverse.currency.model.repositories.ExchangeRateRepository;
-import it.moneyverse.grpc.lib.CurrencyRequest;
-import it.moneyverse.grpc.lib.CurrencyResponse;
-import it.moneyverse.grpc.lib.CurrencyServiceGrpc;
+import it.moneyverse.grpc.lib.*;
 import it.moneyverse.test.utils.RandomUtils;
 import jakarta.persistence.EntityManager;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.util.Optional;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -41,13 +48,14 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
       SecurityAutoConfiguration.class,
       DatasourceAutoConfiguration.class,
     })
+@ExtendWith(MockitoExtension.class)
 class CurrencyServerTest {
 
   private CurrencyServiceGrpc.CurrencyServiceBlockingStub stub;
   private ManagedChannel channel;
   private CurrencyServer currencyServer;
   @Autowired private CurrencyRepository currencyRepository;
-  @Autowired private ExchangeRateRepository exchangeRateRepository;
+  @Mock private ExchangeRateRepository exchangeRateRepository;
   @Autowired EntityManager entityManager;
 
   static CurrencyTestContext testContext;
@@ -116,5 +124,112 @@ class CurrencyServerTest {
     CurrencyResponse response = stub.getCurrencyByCode(request);
 
     assertEquals("", response.getIsoCode());
+  }
+
+  @Test
+  void givenSameCurrencies_WhenGetExchangeRate_ThenBuildExchangeResponse() {
+    String currency = RandomUtils.randomCurrency();
+    ExchangeRateRequest request =
+        ExchangeRateRequest.newBuilder()
+            .setFromCurrency(currency)
+            .setToCurrency(currency)
+            .setDate(RandomUtils.randomDate().toString())
+            .build();
+    ExchangeRateResponse response = stub.getExchangeRate(request);
+
+    assertEquals(1.0, response.getRate());
+  }
+
+  @Test
+  void givenFromEurCurrency_WhenGetExchangeRate_ThenBuildExchangeResponse() {
+    String toCurrency = RandomUtils.randomCurrency();
+    LocalDate date = RandomUtils.randomDate();
+    ExchangeRateRequest request =
+        ExchangeRateRequest.newBuilder()
+            .setFromCurrency("EUR")
+            .setToCurrency(toCurrency)
+            .setDate(date.toString())
+            .build();
+    ExchangeRate exchangeRate = new ExchangeRate();
+    exchangeRate.setRate(RandomUtils.randomBigDecimal());
+    when(exchangeRateRepository.findExchangeRateByCurrencyFromAndCurrencyToAndDate(
+            request.getFromCurrency(), request.getToCurrency(), date))
+        .thenReturn(Optional.of(exchangeRate));
+    ExchangeRateResponse response = stub.getExchangeRate(request);
+
+    assertEquals(exchangeRate.getRate().doubleValue(), response.getRate());
+  }
+
+  @Test
+  void givenToEurCurrency_WhenGetExchangeRate_ThenBuildExchangeResponse() {
+    String fromCurrency = RandomUtils.randomCurrency();
+    LocalDate date = RandomUtils.randomDate();
+    ExchangeRateRequest request =
+        ExchangeRateRequest.newBuilder()
+            .setFromCurrency(fromCurrency)
+            .setToCurrency("EUR")
+            .setDate(date.toString())
+            .build();
+    ExchangeRate exchangeRate = new ExchangeRate();
+    exchangeRate.setRate(RandomUtils.randomBigDecimal());
+    when(exchangeRateRepository.findExchangeRateByCurrencyFromAndCurrencyToAndDate(
+            request.getFromCurrency(), request.getToCurrency(), date))
+        .thenReturn(Optional.of(exchangeRate));
+    ExchangeRateResponse response = stub.getExchangeRate(request);
+
+    assertEquals(
+        BigDecimal.ONE.divide(exchangeRate.getRate(), 4, RoundingMode.HALF_UP).doubleValue(),
+        response.getRate());
+  }
+
+  @Test
+  void givenExchangeRate_WhenGetExchangeRate_ThenBuildExchangeResponse() {
+    String fromCurrency = RandomUtils.randomCurrency();
+    String toCurrency = RandomUtils.randomCurrency();
+    LocalDate date = RandomUtils.randomDate();
+    ExchangeRateRequest request =
+        ExchangeRateRequest.newBuilder()
+            .setFromCurrency(fromCurrency)
+            .setToCurrency(toCurrency)
+            .setDate(date.toString())
+            .build();
+
+    ExchangeRate exchangeRateFrom = new ExchangeRate();
+    exchangeRateFrom.setRate(RandomUtils.randomBigDecimal());
+    when(exchangeRateRepository.findExchangeRateByCurrencyFromAndCurrencyToAndDate(
+            "EUR", request.getFromCurrency(), date))
+        .thenReturn(Optional.of(exchangeRateFrom));
+    ExchangeRate exchangeRateTo = new ExchangeRate();
+    exchangeRateTo.setRate(RandomUtils.randomBigDecimal());
+    when(exchangeRateRepository.findExchangeRateByCurrencyFromAndCurrencyToAndDate(
+            "EUR", request.getToCurrency(), date))
+        .thenReturn(Optional.of(exchangeRateTo));
+    ExchangeRateResponse response = stub.getExchangeRate(request);
+
+    assertEquals(
+        exchangeRateTo
+            .getRate()
+            .divide(exchangeRateFrom.getRate(), RoundingMode.HALF_UP)
+            .doubleValue(),
+        response.getRate());
+  }
+
+  @Test
+  void givenExchangeRate_WhenGetExchangeRate_ThenBuildExchangeResponseWithZeroRate() {
+    String fromCurrency = RandomUtils.randomCurrency();
+    String toCurrency = RandomUtils.randomCurrency();
+    LocalDate date = RandomUtils.randomDate();
+    ExchangeRateRequest request =
+        ExchangeRateRequest.newBuilder()
+            .setFromCurrency(fromCurrency)
+            .setToCurrency(toCurrency)
+            .setDate(date.toString())
+            .build();
+    when(exchangeRateRepository.findExchangeRateByCurrencyFromAndCurrencyToAndDate(
+            "EUR", request.getFromCurrency(), date))
+        .thenReturn(Optional.empty());
+    ExchangeRateResponse response = stub.getExchangeRate(request);
+
+    assertEquals(0.0, response.getRate());
   }
 }
