1af93401af09fb9636989aa29056c2478e0f61cb
Lorenzo Fratini <lorenzo.fratini.developer@gmail.com>
2025-09-14 11:11:01 +0200

    [frontend] feat: added ci pipeline frontend

--- moneyverse-fe/src/environments/environment.ts
+++ moneyverse-fe/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   keycloak: {
-    url: 'http://localhost:7080',
+    url: 'http://moneyverse-keycloak:7080',
     realm: 'moneyverse-fe',
     clientId: 'web-client',
     redirectUri: window.location.origin
@@ -9,7 +9,7 @@ export const environment = {
     expirationTime: 24 * 60 * 60 * 1000, // 1 day
   },
   services: {
-    krakendUrl: 'http://localhost:8080',
-    nginxUrl: 'http://localhost:80'
+    krakendUrl: 'http://moneyverse-krakend:8080',
+    nginxUrl: 'http://moneyverse-nginx:80'
   }
 }
